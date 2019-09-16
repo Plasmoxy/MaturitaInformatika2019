04dae298044439895332944a10019af2c8b8679f
Plasmoxy <sebohacker@gmail.com>
2019-09-16 11:03:17 +0200

    cgood

--- python/faktorial.py
+++ python/faktorial.py
@@ -2,5 +2,7 @@ from functools import reduce
 
 n = int(input("Cislo: "))
 fakt = reduce(lambda a,x: a*x, range(1, n+1))
+neparnesucet = reduce(lambda a,x: a+x, range(1, n+1, 2))
 
 print(f"faktorial -> {fakt}")
+print(f"sucet neparnych -> {neparnesucet}")
