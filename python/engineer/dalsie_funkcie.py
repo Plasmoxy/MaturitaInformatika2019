2c776e7ed30c4130950daebccc511352f54ba53f
Plasmoxy <sebohacker@gmail.com>
2019-12-09 09:38:48 +0100

    Create dalsie_funkcie.py

---
+++ python/engineer/dalsie_funkcie.py
@@ -0,0 +1,10 @@
+from math import pi
+"xdkysxd".count("xd") # == 2
+"kysxd".find("xd") # == 3 (index)
+"xd  asd".strip() # odstrani medzetry na konci a zaciatku a ine oddelovace
+
+# formatovanie je dolezite
+# vediet pouzivat dvojbodku formatovanie
+"{} fuck {}".format(3, 2)
+
+print(f"{pi:0.2f}")
