d1a3c289b254c9349f1eac2bdc73ec498b0e2d11
Plasmoxy <sebohacker@gmail.com>
2019-12-18 08:51:43 +0100

    xd

---
+++ python/strukturovane_typy/bubble_retard_sort_visual.py
@@ -0,0 +1,99 @@
+from tkinter import *
+from typing import List
+from random import randint, sample, randrange
+from colorsys import hsv_to_rgb
+
+W = 1000
+H = 300
+root = Tk()
+
+c = Canvas(root, width=W, height=H, bg="black")
+c2 = Canvas(root, width=W, height=H, bg="black")
+c.pack()
+c2.pack()
+
+# minimum v <a, b>
+def minimumpos(l: List[int], a: int, b: int):
+    mpoz = a
+    for i in range(a, b):
+        if l[i] < l[mpoz]:
+            mpoz = i
+    return mpoz
+
+def draw_list(c: Canvas, l: List[int]):
+    global W, H
+
+    lenl = len(l)
+    maxl = max(l)
+    dx = W/lenl
+    dy = H/maxl
+
+    for i in range(0, lenl):
+        kolor = "#%02x%02x%02x" % tuple(map(lambda t: int(255*t), hsv_to_rgb(l[i]/maxl, 1.0, 1.0)))
+        c.create_rectangle(i*dx, H, i*dx + dx, H - l[i]*dy, fill=kolor, outline=kolor)
+
+    
+# sample zoznam cisel pre sortovaci algoritmus
+ls = [randrange(0, 300) for i in range(0, 300)]
+ls_i = 0
+ls_last = 0
+ls_len = len(ls)
+sort_active = True
+
+# sample pre 2. algoritmus
+ls2 = ls.copy()
+ls2_i = 0
+
+
+def loop():
+    global root, c, c2
+    c.delete("all")
+    c2.delete("all")
+
+    ####################################################
+    # SORT ALG 1 ITER START
+    global ls, ls_i, sort_active, ls_last, ls_len
+    
+    vymeny = 0
+    for i in range(1, ls_len-ls_last):
+        # ak cislo nalavo je vacsie ako cislo napravo
+        if ls[i-1] > ls[i]:
+            ls[i], ls[i-1] = ls[i-1], ls[i]
+            vymeny += 1
+    
+    ls_last += 1
+
+    # display
+    draw_list(c, ls)
+    
+    # ukonci ak nebolo nic vymenene
+    if vymeny == 0:
+        sort_active = False # stop sort
+
+    # SORT ALG 1 ITER END
+    ####################################################
+
+    ####################################################
+    # SORT ALG 2 ITER START
+    global ls2, ls2_i
+
+    if ls2_i < ls_len:
+        # najdi najmensie cislo v rozsahu <i, len-1)
+        x = minimumpos(ls2, ls2_i, ls_len)
+
+        # trepni ho na zaciatok
+        ls2[ls2_i], ls2[x] = ls2[x], ls2[ls2_i]
+
+        # pokracuj dalej
+        ls2_i += 1
+
+    draw_list(c2, ls2)
+
+    # SORT ALG 2 ITER END
+    ####################################################
+
+    root.update()
+    root.after(1, loop)
+
+loop()
+root.mainloop()
--- python/strukturovane_typy/bubble_sort.py
+++ python/strukturovane_typy/bubble_sort.py
@@ -2,4 +2,40 @@
 Stale menim dva medzi sebou nasledujuce.
 Cisla bublaju na koniec.
 Ak uz nebude ziadna vymena = koniec = sorted.
+Posledny uz nema kontrolovat potom neskor.
 """
+
+from random import sample, randint
+from typing import *
+
+def bublo_srot(l: List[int]) -> List[int]:
+    ll = len(l)
+    iter_counter = 0 # porovnania
+    last = 0 # ak posledny je uz zosortovany, nechaj tak
+
+    while True:
+        
+        vymeny = 0
+        for i in range(1, ll-last):
+            iter_counter += 1
+            # ak cislo nalavo je vacsie ako cislo napravo
+            if l[i-1] > l[i]:
+                l[i], l[i-1] = l[i-1], l[i]
+                vymeny += 1
+            
+        # ak dana hodnota uz dobublala (sme na konci), limituj dlzku aby sme uz neoverovali posledny prvok
+        last += 1
+
+        print(f"bubble sort l = {l}, hranica ukonceneho sortovania(ll-last) = {ll-last}")
+        
+        # ukonci ak nebolo nic vymenene
+        if vymeny == 0:
+            print(f"[Prebehlo {iter_counter} iteracii (vymen).]")
+            break
+    
+    return l
+
+lcisla = [randint(0, 10) for i in range(0, 11)]
+print(f"Cisla = {lcisla}, len={len(lcisla)}")
+
+print(f"Sorted = {bublo_srot(lcisla)}")
