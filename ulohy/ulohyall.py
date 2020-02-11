76a5af81cb4b3fb8ea355884f2c2d0fdd236d948
Plasmoxy <sebohacker@gmail.com>
2020-02-11 19:42:23 +0100

    Update ulohyall.py

--- ulohy/ulohyall.py
+++ ulohy/ulohyall.py
@@ -189,3 +189,4 @@ print("=== Uloha [30] ===")
 ===================================
 ### Napíšte program, ktorý zašifruje vetu zadanú užívateľom tak, že znaky v zašifrovanej vete budú posunuté o jeden znak v ASCII tabuľke vpravo. Zašifrovanú vetu vypíše a vypíše ju aj naopak - od posledného znaku k prvému.
 ### (Príklad: AHOJ sa zašifruje na BIPK)  
+
