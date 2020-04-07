1bbd1d4cbd60141f5cf0ceaed859fc0aa069160b
Plasmoxy <sebohacker@gmail.com>
2020-04-07 14:02:12 +0200

    Update u15.py

--- ulohyPL/u15.py
+++ ulohyPL/u15.py
@@ -10,23 +10,26 @@ print(f"predtym: {l}")
 sl = sorted(l)
 print(f"pomocou funkcie sorted: {sl}")
 
-
 # AVŠAK zrejme bude treba implementovať bez sorted
-# nie je napisane presne aky algoritmus treba, bude stacit ist podla minima
+# nie je napisane presne aky algoritmus treba, ja zrobim cez bubble sort
+# budem postupovat od zaciatku (i=0) po koniec, hodnoty teda budu bublovat tymto smerom
 
-# ziska index najmensieho cisla v rozsahu
-def minimum_i(zoznam, a, b):
-  mi = a # nastavim na prvy prvok
-  for i in range(a, b):
-    if zoznam[i] < zoznam[mi]:
-      mi = i
-  return mi
+koniec = len(l)-1 # na zaciatku ideme cez cely zoznam, postupne potom znizujeme koniec
 
-ia = 0
-ib = len(l) - 1
+while True:
+  vymeny = 0 # overujeme pocet vymen
+
+  # nevratane konca, lebo ak sme na koniec-1, tak potom overujeme koniec-1 a koniec (lebo i+1)
+  for i in range(0, koniec):
+    # ak je i vacsie ako i+1
+    if l[i] > l[i+1]:
+      l[i], l[i+1] = l[i+1], l[i] # vymen ich (vybublaj)
+      vymeny += 1
+
+  if vymeny == 0: 
+    break # ak uz nic nebubla, ukonci bublanie
+  else:
+    koniec -= 1 # toto nie je nevyhnutne; zniz koniec (pretoze vybublala na koniec hodnota a ta uz dalej bublat vyssie nebude)
 
 
-
-# -> alebo jednoduchsie cez l.index(min(l))
-
-print(minimum_i(l))
+print(f"Posortovany pomocou bubble sortu: {l}")
