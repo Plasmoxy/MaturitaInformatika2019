f737acd9f5d8015bd5d70461a5129589c917f8a4
Plasmoxy <sebohacker@gmail.com>
2020-04-20 19:06:40 +0200

    Update u24.py

--- ulohyPL/u24.py
+++ ulohyPL/u24.py
@@ -1,3 +1,10 @@
 # === Úloha 24===
 # Napíšte program, ktorý zistí koľko je v súbore vzor.txt riadkov aj znakov a taktiež  určí počet znakov v najdlhšom riadku.
+from functools import reduce
 
+with open("text.txt", "r") as f:
+  t = f.readlines()
+najdlh = max(list(map(lambda riadok: len(riadok), t)))
+znakov = reduce(lambda a, x: a + len(x), t, 0)
+print(f"Pocet riadkov: {len(t)}, pocet znakov: {znakov}")
+print(f"Najdlhsi riadok ma {najdlh} znakov.")
