1f0bcce839d3a00e558e934bcfd91e6aa4d0783a
Plasmoxy <sebohacker@gmail.com>
2020-04-21 15:22:12 +0200

    Update u12.py

--- ulohyPL/u12.py
+++ ulohyPL/u12.py
@@ -1,6 +1,7 @@
 # === Úloha 12===
 # Syn dostával od otca v roku 2008 pevné vreckové N eur každý týždeň. Od januára 2009 mu otec zvyšuje vreckové každý týždeň o 0,2€. Napíšte program, ktorý vypíše postupne koľko otec vyplatí synovi na vreckovom každý týždeň počas roka a koľko spolu za celý rok. Výdavky vypíše program na obrazovku a do textového súboru po 5 týždňoch do jedného riadka.
 
-
+# Rok ma 52 tyzdnov.
 N = float(input("Zadajte pevne vreckove v roku 2008: "))
+
 
